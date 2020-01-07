d337f5fea619dc37b56cfe3783e04b8f6e99988a
StefanMelich <melich.stefan@centrum.sk>
2020-01-07 14:13:57 +0100

    Added keyboard controller

--- Pong/GameBoard.h
+++ Pong/GameBoard.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <conio.h>
 #include "Player.h"
 #include "Ball.h"
 
@@ -9,21 +10,30 @@ class GameBoard
 {
 private:
 	int width, height;
+	char up, down;
+	bool quit;
 	Ball * ball;
 	Player * player1;
 	Player * player2;
+
+	bool canDrawPlayer(int actualX, int actualY, int x, int y);
 
 public:
 	GameBoard(int w, int h);
 	~GameBoard();
 	void draw();
-
+	void input();
+	void collisionDetection();
+	void run();
 };
 
 inline GameBoard::GameBoard(int w, int h)
 {
 	width = w;
 	height = h;
+	up = 'w';
+	down = 's';
+	quit = false;
 	ball = new Ball(width / 2, height / 2);
 	player1 = new Player(1, (height / 2) - 3);
 	player2 = new Player(width - 2, (height / 2) - 3);
@@ -63,12 +73,12 @@ inline void GameBoard::draw()
 			if (ballX == j && ballY == i)
 				cout << "O";
 
-			// player1
-			else if (p1X == j && p1Y == i)
+			//player1
+			else if (canDrawPlayer(j, i, p1X, p1Y))
 				cout << "\xDB";
 
-			// player2
-			else if (p2X == j && p2Y == i)
+			//player2
+			else if (canDrawPlayer(j, i, p2X, p2Y))
 				cout << "\xDB";
 
 			// empty space
@@ -88,5 +98,89 @@ inline void GameBoard::draw()
 	cout << endl;
 }
 
+inline void GameBoard::input()
+{
+	ball->move();
+
+	if (_kbhit())
+	{
+		char current = _getch();
+
+		if (current == up)
+		{
+			if (player1->getY() > 0)
+			{
+				player1->moveUP();
+			}
+		}
+
+		if (current == down)
+		{
+			if (player1->getY() + 4 < height)
+			{
+				player1->moveDOWN();
+			}
+		}
+
+		if (current == 'q')
+		{
+			quit = true;
+		}
+	}
+}
+
+inline void GameBoard::collisionDetection()
+{
+	int ballX = ball->getX();
+	int ballY = ball->getY();
+	int p1X = player1->getX();
+	int p1Y = player1->getY();
+
+	// SERVER - left player 
+	for (int i = 0; i < 4; i++)
+	{
+		if (ballX == p1X + 1)
+		{
+			if (ballY == p1Y + i)
+			{
+				ball->rndRight();
+			}
+		}
+	}
+
+	// CLIENT - right player
+	// TODO: tcp 
+
+	// ball hit wall bounce
+	if (ballY == height - 1 || ballY == 0)
+	{
+		ball->wallChangeDir();
+	}
+
+	// ball hit wall game over
+	//if (ballX == width - 1)
 
 
+	//if (ballX == 0)
+
+}
+
+inline void GameBoard::run()
+{
+	while(!quit)
+	{
+		draw();
+		input();
+		collisionDetection();
+	}
+}
+
+inline bool GameBoard::canDrawPlayer(int actualX, int actualY, int x, int y)
+{
+	if (actualX == x)
+	{
+		if (actualY == y || actualY == y + 1 || actualY == y + 2 || actualY == y + 3)
+			return true;
+	}
+	return false;
+}
